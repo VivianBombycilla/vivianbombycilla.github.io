4028917fb5b4697e533e60c438b9f19bc647860b
vbbcla <83726864+VivianBombycilla@users.noreply.github.com>
2025-09-18 22:56:01 +0100

    initial commit

---
+++ components/header.js
@@ -0,0 +1,41 @@
+const headerTemplate = document.createElement('template');
+
+headerTemplate.innerHTML = `
+    <style>
+    ul {
+        list-style: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    li {
+        display: inline;
+    }
+    </style>
+    <header>
+    <nav>
+        <ul id="navigation-menu">
+            <li>
+                <a href="index.html">Home</a>
+            </li>
+            <li>
+                <a href="blog.html">Blog</a>
+            </li>
+        </ul>
+    </nav>
+    </header>
+`;
+
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        // Attach shadow root to custom component
+        const shadowRoot = this.attachShadow({ mode: 'closed' });
+        shadowRoot.appendChild(headerTemplate.content);
+    }
+}
+
+customElements.define('header-component', Header);
