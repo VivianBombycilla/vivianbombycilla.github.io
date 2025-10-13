374aaa1d3a104c39cddefaeb468bae8b93faa117
vbbcla <83726864+VivianBombycilla@users.noreply.github.com>
2025-10-13 22:21:37 +0100

    new navbar, page

--- public/components/header.js
+++ public/components/header.js
@@ -3,11 +3,38 @@ const headerTemplate = document.createElement('template');
 headerTemplate.innerHTML = `
     <style>
     ul.nav-menu {
-        list-style: none;
-        margin: 0;
         padding: 0;
+        height: 2em;
+        transform: rotate(-90deg) translateY(100%);
+        transform-origin: bottom left;
         li {
-            display: inline;
+            width: max-content;
+            margin: 0;
+            padding: 0;
+            transform: rotate(30deg);
+            transform-origin: bottom left;
+            font-size: 12pt;
+        }
+        li::marker {
+            font-size: 17pt
+        }
+        li:before {
+            background-color: #000;
+            width: 2px;
+            content: '';
+            position: absolute;
+            top: 0px;
+            bottom: 0px;
+            left: -7.93px;
+            transform: rotate(-30deg);
+            transform-origin: bottom left;
+            
+        }
+        li:first-child:before {
+            top: 13px;
+        }
+        li:last-child:before {
+            transform: rotate(-30deg) translateY(-13px);
         }
     }
     </style>
@@ -25,6 +52,9 @@ headerTemplate.innerHTML = `
             </li>
             <li>
                 <a href="/games.html">Games</a>
+            </li>
+            <li>
+                <a href="/tools.html">Tools</a>
             </li>
         </ul>
     </nav>
